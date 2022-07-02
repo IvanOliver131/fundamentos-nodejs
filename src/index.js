af87ff2fd3ac5bf20a1103fc289d2e79de651ead
Ivan Lucas de Oliveira Ribeiro <ivanoliver131@gmail.com>
2022-07-02 11:45:22 -0300

    hello world🚀

--- src/index.js
+++ src/index.js
@@ -2,4 +2,10 @@ const express = require("express");
 
 const app = express();
 
-app.use();
+app.get("/", (request, response) => {
+  return response.json({
+    message: "Hello World Ignite!"
+  });
+});
+
+app.listen(3333);
