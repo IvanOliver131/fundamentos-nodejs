b2d7fa9eef1f356fb95c81d128aa017c590832a2
Ivan Lucas de Oliveira Ribeiro <ivanoliver131@gmail.com>
2022-07-02 12:24:27 -0300

    devCOmmit

--- src/index.js
+++ src/index.js
@@ -2,23 +2,36 @@ const express = require("express");
 
 const app = express();
 
+// importamos isso para aceitar receber um objeto JSON
+app.use(express.json());
+
 app.get("/courses", (request, response) => {
+  const query = request.query;
+  console.log(query);
   return response.json(["Curso 1", "Curso 2", "Curso 3"]);
 });
 
 app.post("/courses", (request, response) => {
+  const body = request.body;
+  console.log(body);
   return response.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"]);
 });
 
 app.put("/courses/:id", (request, response) => {
+  const params = request.params;
+  console.log(params);
   return response.json(["Curso 6", "Curso 2", "Curso 3", "Curso 4"]);
 });
 
 app.patch("/courses/:id", (request, response) => {
+  const params = request.params;
+  console.log(params);
   return response.json(["Curso 6", "Curso 7", "Curso 3", "Curso 4"]);
 });
 
 app.delete("/courses/:id", (request, response) => {
+  const params = request.params;
+  console.log(params);
   return response.json(["Curso 6", "Curso 7", "Curso 4"]);
 });
 
